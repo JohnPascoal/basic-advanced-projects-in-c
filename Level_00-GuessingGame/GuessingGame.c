8faf186f9b836605a21353010fc69d9a6245ec2a
John Pascoal <johnedgar1@hotmail.com>
2025-05-05 00:31:29 +0100

    4º cmt - Feat: add rand function

--- Level_00-GuessingGame/GuessingGame.c
+++ Level_00-GuessingGame/GuessingGame.c
@@ -2,22 +2,37 @@
 // Arquivo: GuessingGame.c
 // Autor: John Pascoal
 // Criado em: 2025-05-04 às 22:16:29
-// Actualizado em: 2025-05-04 às 23:20:30
+// Actualizado em: 2025-05-05 às 00:27:48
 // -----------------------------------------------------------------------------
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MIN 1
+#define MAX 10
+
+int	rand_secret_number(void)
+{
+	srand(time(NULL));
+
+	return ((rand() % (MAX - MIN + 1)) + MIN);
+}
 
 int	main(void)
 {
 	int	secretNumber;
 	int	guess;
 
-	secretNumber = 6;
+	secretNumber = rand_secret_number();
+
+	printf("Guess the correct number between %d and %d!\n", MIN, MAX);
 	do {
-	printf("Enter a number: ");
-	scanf("%d", &guess);
-	}while(guess != secretNumber);
+		printf("Enter a number: ");
+		scanf("%d", &guess);
+	} while (guess != secretNumber);
 	
-	printf("you won, the correct number is %d!", secretNumber);
+	printf("You won, the correct number is %d!", secretNumber);
+	
 	return (0);
 }
