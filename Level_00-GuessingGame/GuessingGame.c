f1c73f51ebef4a7d80632fdc87897c4b3ecc97d2
John Pascoal <johnedgar1@hotmail.com>
2025-05-05 11:09:59 +0100

    feat: create function run
    organize the function main, separating the code to function run

--- Level_00-GuessingGame/GuessingGame.c
+++ Level_00-GuessingGame/GuessingGame.c
@@ -2,7 +2,7 @@
 // Arquivo: GuessingGame.c
 // Autor: John Pascoal
 // Criado em: 2025-05-04 às 22:16:29
-// Actualizado em: 2025-05-05 às 09:38:55
+// Actualizado em: 2025-05-05 às 11:02:36
 // -----------------------------------------------------------------------------
 
 #include <stdio.h>
@@ -12,44 +12,50 @@
 #define MIN 1
 #define MAX 100
 
-int	rand_secret_number(void)
+void	run(int guess_number, int check_type, int secret_number, int *attempt)
 {
-	return ((rand() % (MAX - MIN + 1)) + MIN);
+	do {
+		printf("Enter a number: ");
+		check_type = scanf("%d", &guess_number);
+		if (check_type != 1)
+		{
+			printf("\'The number must be a digit between %d and %d\'\n", MIN, MAX);
+			while (getchar() != '\n');
+			guess_number = -1;
+		}
+		else
+		{
+			if (guess_number != secret_number)
+			{
+				printf("Attempt: %d\n", --*attempt);
+				if (guess_number > secret_number)
+					printf("\'The secret number is less\'\n");
+				else
+					printf("\'The secret number is greater\'\n");
+			}
+		}
+	} while (*attempt && guess_number != secret_number);	
 }
 
 int	main(void)
 {
-	int	secretNumber;
-	int	guess;
+	int	secret_number;
+	int	guess_number;
 	int	check_type;
+	int	attempt;
 
 	srand(time(NULL));
-	secretNumber = rand_secret_number();
+	secret_number = (rand() % (MAX - MIN + 1)) + MIN;
+	check_type = 1;
+	guess_number = -1;
+	attempt = 10;
 
-	printf("\"Guess the secret number between %d and %d\"\n", MIN, MAX);
-	do {
-		printf("Enter a number: ");
-		check_type = scanf("%d", &guess);
-
-		if (check_type != 1)
-		{
-			printf("Put a number between %d and %d\n", MIN, MAX);
-			while (getchar() != '\n');
-            		guess = -1; 
-		}
-		else
-		{
-			if (guess != secretNumber)
-			{
-				if (guess > secretNumber)
-					printf("\"The secret number is less\"\n");
-				else
-					printf("\"The secret number is greater\"\n");
-			}
-		}
-	} while (guess != secretNumber);
-	
-	printf("You won, the correct number is %d!", secretNumber);
+	printf("\'Guess the secret number between %d and %d\'\n", MIN, MAX);
+	run(guess_number, check_type, secret_number, &attempt);
+	if (attempt != 0)
+		printf("You won, the correct number is %d!", secret_number);
+	else
+		printf("You lost, try again!");
 	
 	return (0);
 }
